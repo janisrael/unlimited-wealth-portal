4fa777e9dbc1e2b587d1edd1e2a37634b21c8d3f
Jan Israel <jan.israel@smartchartsfx.com>
2023-06-16 15:54:23 +0800

    modal

--- src/components/LeftContent.vue
+++ src/components/LeftContent.vue
@@ -2,157 +2,215 @@
   <div class="left-panel-wrapper">
     <el-row>
       <el-col :span="24" style="padding: 0px 20px !important;">
-        <el-col :span="8" style="padding-right: 20px;padding-top: 20px;">
-          <el-card class="box-card card-left-panel" shadow="hover">
-            <div slot="header" class="clearfix">
-              <div class="card-header-content"  :style="{ backgroundImage: 'url(' + thumbnail_image + ')' }">
-                  <div class="card-content-title">LFX Daily webinar</div>    
-                  <div class="card-content-region">Europe</div>    
-                  <div class="card-content-sched">Coming up at 3rd April</div>    
+        <el-col :span="8" style="padding-right: 20px;padding-top: 20px;" >
+          <div @click="getModal()">
+            <el-card class="box-card card-left-panel" shadow="hover" >
+              <div slot="header" class="clearfix">
+                <div class="card-header-content"  :style="{ backgroundImage: 'url(' + thumbnail_image + ')' }">
+                    <div class="card-content-title">LFX Daily webinar 1</div>    
+                    <div class="card-content-region">Europe</div>    
+                    <div class="card-content-sched">Coming up at 3rd April</div>    
+                </div>
+                
+                <!-- <el-button style="float: right; padding: 3px 0" type="text">Operation button</el-button> -->
               </div>
-              
-              <!-- <el-button style="float: right; padding: 3px 0" type="text">Operation button</el-button> -->
-            </div>
-            <div  class="text item">
-              Get ahead of the curve with our early morning briefing. Our trader mentors will help you look trough the markets to see what’s going on and some places that you might want to look for today’s trades. 
-            </div>
-          </el-card>
+              <div  class="text item">
+                Get ahead of the curve with our early morning briefing. Our trader mentors will help you look trough the markets to see what’s going on and some places that you might want to look for today’s trades. 
+              </div>
+            </el-card>
+          </div>
         </el-col>
         <el-col :span="8"  style="padding-right: 20px;padding-top: 20px;">
-          <el-card class="box-card card-left-panel" shadow="hover">
-            <div slot="header" class="clearfix">
-              <div class="card-header-content"  :style="{ backgroundImage: 'url(' + thumbnail_image + ')' }">
-                  <div class="card-content-title">LFX Daily webinar</div>    
-                  <div class="card-content-region">Europe</div>    
-                  <div class="card-content-sched">Coming up at 3rd April</div>    
+          <div @click="getModal()">
+            <el-card class="box-card card-left-panel" shadow="hover" >
+              <div slot="header" class="clearfix">
+                <div class="card-header-content"  :style="{ backgroundImage: 'url(' + thumbnail_image + ')' }">
+                    <div class="card-content-title">LFX Daily webinar 1</div>    
+                    <div class="card-content-region">Europe</div>    
+                    <div class="card-content-sched">Coming up at 3rd April</div>    
+                </div>
+                
+                <!-- <el-button style="float: right; padding: 3px 0" type="text">Operation button</el-button> -->
               </div>
-              
-              <!-- <el-button style="float: right; padding: 3px 0" type="text">Operation button</el-button> -->
-            </div>
-            <div  class="text item">
-              Get ahead of the curve with our early morning briefing. Our trader mentors will help you look trough the markets to see what’s going on and some places that you might want to look for today’s trades. 
-            </div>
-          </el-card>
+              <div  class="text item">
+                Get ahead of the curve with our early morning briefing. Our trader mentors will help you look trough the markets to see what’s going on and some places that you might want to look for today’s trades. 
+              </div>
+            </el-card>
+          </div>
         </el-col>
         <el-col :span="8"  style="padding-right: 20px;padding-top: 20px;">
-          <el-card class="box-card card-left-panel" shadow="hover">
-            <div slot="header" class="clearfix">
-              <div class="card-header-content"  :style="{ backgroundImage: 'url(' + thumbnail_image + ')' }">
-                  <div class="card-content-title">LFX Daily webinar</div>    
-                  <div class="card-content-region">Europe</div>    
-                  <div class="card-content-sched">Coming up at 3rd April</div>    
+          <div @click="getModal()">
+            <el-card class="box-card card-left-panel" shadow="hover" >
+              <div slot="header" class="clearfix">
+                <div class="card-header-content"  :style="{ backgroundImage: 'url(' + thumbnail_image + ')' }">
+                    <div class="card-content-title">LFX Daily webinar 1</div>    
+                    <div class="card-content-region">Europe</div>    
+                    <div class="card-content-sched">Coming up at 3rd April</div>    
+                </div>
+                
+                <!-- <el-button style="float: right; padding: 3px 0" type="text">Operation button</el-button> -->
               </div>
-            </div>
-            <div  class="text item">
-              Get ahead of the curve with our early morning briefing. Our trader mentors will help you look trough the markets to see what’s going on and some places that you might want to look for today’s trades. 
-            </div>
-          </el-card>
-        </el-col>
-
-        <el-col :span="8"  style="padding-right: 20px;padding-top: 20px;">
-          <el-card class="box-card card-left-panel" shadow="hover">
-            <div slot="header" class="clearfix">
-              <div class="card-header-content"  :style="{ backgroundImage: 'url(' + thumbnail_image + ')' }">
-                  <div class="card-content-title">LFX Daily webinar</div>    
-                  <div class="card-content-region">Europe</div>    
-                  <div class="card-content-sched">Coming up at 3rd April</div>    
+              <div  class="text item">
+                Get ahead of the curve with our early morning briefing. Our trader mentors will help you look trough the markets to see what’s going on and some places that you might want to look for today’s trades. 
               </div>
-            </div>
-            <div  class="text item">
-              Get ahead of the curve with our early morning briefing. Our trader mentors will help you look trough the markets to see what’s going on and some places that you might want to look for today’s trades. 
-            </div>
-          </el-card>
+            </el-card>
+          </div>
         </el-col>
 
         <el-col :span="8"  style="padding-right: 20px;padding-top: 20px;">
-          <el-card class="box-card card-left-panel" shadow="hover">
-            <div slot="header" class="clearfix">
-              <div class="card-header-content"  :style="{ backgroundImage: 'url(' + thumbnail_image + ')' }">
-                  <div class="card-content-title">LFX Daily webinar</div>    
-                  <div class="card-content-region">Europe</div>    
-                  <div class="card-content-sched">Coming up at 3rd April</div>    
+          <div @click="getModal()">
+            <el-card class="box-card card-left-panel" shadow="hover" >
+              <div slot="header" class="clearfix">
+                <div class="card-header-content"  :style="{ backgroundImage: 'url(' + thumbnail_image + ')' }">
+                    <div class="card-content-title">LFX Daily webinar 1</div>    
+                    <div class="card-content-region">Europe</div>    
+                    <div class="card-content-sched">Coming up at 3rd April</div>    
+                </div>
+                
+                <!-- <el-button style="float: right; padding: 3px 0" type="text">Operation button</el-button> -->
               </div>
-            </div>
-            <div  class="text item">
-              Get ahead of the curve with our early morning briefing. Our trader mentors will help you look trough the markets to see what’s going on and some places that you might want to look for today’s trades. 
-            </div>
-          </el-card>
+              <div  class="text item">
+                Get ahead of the curve with our early morning briefing. Our trader mentors will help you look trough the markets to see what’s going on and some places that you might want to look for today’s trades. 
+              </div>
+            </el-card>
+          </div>
         </el-col>
 
-
         <el-col :span="8"  style="padding-right: 20px;padding-top: 20px;">
-          <el-card class="box-card card-left-panel" shadow="hover">
-            <div slot="header" class="clearfix">
-              <div class="card-header-content"  :style="{ backgroundImage: 'url(' + thumbnail_image + ')' }">
-                  <div class="card-content-title">LFX Daily webinar</div>    
-                  <div class="card-content-region">Europe</div>    
-                  <div class="card-content-sched">Coming up at 3rd April</div>    
+          <div @click="getModal()">
+            <el-card class="box-card card-left-panel" shadow="hover" >
+              <div slot="header" class="clearfix">
+                <div class="card-header-content"  :style="{ backgroundImage: 'url(' + thumbnail_image + ')' }">
+                    <div class="card-content-title">LFX Daily webinar 1</div>    
+                    <div class="card-content-region">Europe</div>    
+                    <div class="card-content-sched">Coming up at 3rd April</div>    
+                </div>
+                
+                <!-- <el-button style="float: right; padding: 3px 0" type="text">Operation button</el-button> -->
               </div>
-            </div>
-            <div  class="text item">
-              Get ahead of the curve with our early morning briefing. Our trader mentors will help you look trough the markets to see what’s going on and some places that you might want to look for today’s trades. 
-            </div>
-          </el-card>
-        </el-col>
-
-
-        <el-col :span="8"  style="padding-right: 20px;padding-top: 20px;">
-          <el-card class="box-card card-left-panel" shadow="hover">
-            <div slot="header" class="clearfix">
-              <div class="card-header-content"  :style="{ backgroundImage: 'url(' + thumbnail_image + ')' }">
-                  <div class="card-content-title">LFX Daily webinar</div>    
-                  <div class="card-content-region">Europe</div>    
-                  <div class="card-content-sched">Coming up at 3rd April</div>    
+              <div  class="text item">
+                Get ahead of the curve with our early morning briefing. Our trader mentors will help you look trough the markets to see what’s going on and some places that you might want to look for today’s trades. 
               </div>
-            </div>
-            <div  class="text item">
-              Get ahead of the curve with our early morning briefing. Our trader mentors will help you look trough the markets to see what’s going on and some places that you might want to look for today’s trades. 
-            </div>
-          </el-card>
+            </el-card>
+          </div>
         </el-col>
 
 
         <el-col :span="8"  style="padding-right: 20px;padding-top: 20px;">
-          <el-card class="box-card card-left-panel" shadow="hover">
-            <div slot="header" class="clearfix">
-              <div class="card-header-content"  :style="{ backgroundImage: 'url(' + thumbnail_image + ')' }">
-                  <div class="card-content-title">LFX Daily webinar</div>    
-                  <div class="card-content-region">Europe</div>    
-                  <div class="card-content-sched">Coming up at 3rd April</div>    
+          <div @click="getModal()">
+            <el-card class="box-card card-left-panel" shadow="hover" >
+              <div slot="header" class="clearfix">
+                <div class="card-header-content"  :style="{ backgroundImage: 'url(' + thumbnail_image + ')' }">
+                    <div class="card-content-title">LFX Daily webinar 1</div>    
+                    <div class="card-content-region">Europe</div>    
+                    <div class="card-content-sched">Coming up at 3rd April</div>    
+                </div>
+                
+                <!-- <el-button style="float: right; padding: 3px 0" type="text">Operation button</el-button> -->
               </div>
-            </div>
-            <div  class="text item">
-              Get ahead of the curve with our early morning briefing. Our trader mentors will help you look trough the markets to see what’s going on and some places that you might want to look for today’s trades. 
-            </div>
-          </el-card>
+              <div  class="text item">
+                Get ahead of the curve with our early morning briefing. Our trader mentors will help you look trough the markets to see what’s going on and some places that you might want to look for today’s trades. 
+              </div>
+            </el-card>
+          </div>
         </el-col>
 
 
         <el-col :span="8"  style="padding-right: 20px;padding-top: 20px;">
-          <el-card class="box-card card-left-panel" shadow="hover">
-            <div slot="header" class="clearfix">
-              <div class="card-header-content"  :style="{ backgroundImage: 'url(' + thumbnail_image + ')' }">
-                  <div class="card-content-title">LFX Daily webinar</div>    
-                  <div class="card-content-region">Europe</div>    
-                  <div class="card-content-sched">Coming up at 3rd April</div>    
+          <div @click="getModal()">
+            <el-card class="box-card card-left-panel" shadow="hover" >
+              <div slot="header" class="clearfix">
+                <div class="card-header-content"  :style="{ backgroundImage: 'url(' + thumbnail_image + ')' }">
+                    <div class="card-content-title">LFX Daily webinar 1</div>    
+                    <div class="card-content-region">Europe</div>    
+                    <div class="card-content-sched">Coming up at 3rd April</div>    
+                </div>
+                
+                <!-- <el-button style="float: right; padding: 3px 0" type="text">Operation button</el-button> -->
               </div>
-            </div>
-            <div  class="text item">
-              Get ahead of the curve with our early morning briefing. Our trader mentors will help you look trough the markets to see what’s going on and some places that you might want to look for today’s trades. 
-            </div>
-          </el-card>
+              <div  class="text item">
+                Get ahead of the curve with our early morning briefing. Our trader mentors will help you look trough the markets to see what’s going on and some places that you might want to look for today’s trades. 
+              </div>
+            </el-card>
+          </div>
+        </el-col>
+
+
+        <el-col :span="8"  style="padding-right: 20px;padding-top: 20px;">
+          <div @click="getModal()">
+            <el-card class="box-card card-left-panel" shadow="hover" >
+              <div slot="header" class="clearfix">
+                <div class="card-header-content"  :style="{ backgroundImage: 'url(' + thumbnail_image + ')' }">
+                    <div class="card-content-title">LFX Daily webinar 1</div>    
+                    <div class="card-content-region">Europe</div>    
+                    <div class="card-content-sched">Coming up at 3rd April</div>    
+                </div>
+                
+                <!-- <el-button style="float: right; padding: 3px 0" type="text">Operation button</el-button> -->
+              </div>
+              <div  class="text item">
+                Get ahead of the curve with our early morning briefing. Our trader mentors will help you look trough the markets to see what’s going on and some places that you might want to look for today’s trades. 
+              </div>
+            </el-card>
+          </div>
+        </el-col>
+
+
+        <el-col :span="8"  style="padding-right: 20px;padding-top: 20px;">
+          <div @click="getModal()">
+            <el-card class="box-card card-left-panel" shadow="hover" >
+              <div slot="header" class="clearfix">
+                <div class="card-header-content"  :style="{ backgroundImage: 'url(' + thumbnail_image + ')' }">
+                    <div class="card-content-title">LFX Daily webinar 1</div>    
+                    <div class="card-content-region">Europe</div>    
+                    <div class="card-content-sched">Coming up at 3rd April</div>    
+                </div>
+                
+                <!-- <el-button style="float: right; padding: 3px 0" type="text">Operation button</el-button> -->
+              </div>
+              <div  class="text item">
+                Get ahead of the curve with our early morning briefing. Our trader mentors will help you look trough the markets to see what’s going on and some places that you might want to look for today’s trades. 
+              </div>
+            </el-card>
+          </div>
         </el-col>
       </el-col>
+      <component :is="currentComponent" :type="type" @close="CloseModal()"/>
     </el-row>
   </div>
+  
 </template>
 
 <script>
+  
+  import EventModal from './modal/EventModal.vue'
   export default {
     name: 'LeftContent',
+    components: {
+      EventModal
+    },
+    props: {
+      type: {
+        type: String,
+        required: true
+      },
+    },
     data() {
       return {
-        thumbnail_image: require('../assets/images/visual.png')
+        thumbnail_image: require('../assets/images/visual.png'),
+        currentComponent: null
+      }
+    },
+    methods: {
+      getModal() {
+        // if(this.type === 'upcoming') {
+          this.currentComponent = EventModal
+        // } else {
+        //   this.currentComponent = EventModal
+        // }
+      },
+      CloseModal() {
+          this.currentComponent = null
       }
     },
   }
---
+++ src/components/modal/EventModal.vue
@@ -0,0 +1,135 @@
+<template>
+  <div>
+    <el-dialog
+      title="LFX Daily webinar - Europe"
+      :visible.sync="dialogVisible"
+      width="50%"
+      :before-close="handleClose">
+      <span class="modal-span">Get ahead of the curve with our early morning briefing. Our trader mentors will help you look trough the markets to see what’s going on and some places that you might want to look for today’s trades.</span>
+      <el-col :span="24">
+      <div style="  display: inline-block; width: 100%; text-align: left;  padding: 20px 5px 10px;">
+        <div style="  display: inline-block;margin-right: 10px;">
+          <el-avatar :size="30" src="https://empty">
+            <img src="https://cube.elemecdn.com/e/fd/0fc7d20532fdaf769a25683617711png.png"/>
+          </el-avatar>
+        </div>
+        <div style="  display: inline-block;">
+          <div style="color: #A2B0D5;font-zie: 12px;">Speaker:</div>
+          <div style="color: #ffffff;">Amy Green</div>
+        </div>  
+
+        <div v-if="type === 'recording'" style="  display: inline-block;padding: 0 50px">
+          <div style="color: #A2B0D5;font-zie: 12px;">Time:</div>
+          <div style="color: #ffffff;">09:00 local</div>
+        </div>  
+
+        <div style="  display: inline-block;padding: 0 50px">
+          <div style="color: #A2B0D5;font-zie: 12px;">Region:</div>
+          <div style="color: #ffffff;">UK</div>
+        </div>  
+
+        <div style="  display: inline-block;padding: 0 50px">
+          <div style="color: #A2B0D5;font-zie: 12px;">Duration:</div>
+          <div style="color: #ffffff;">30 minutes</div>
+        </div>  
+        <div v-if="type === 'recording'" style="  display: inline-block;">
+          <div style="color: #A2B0D5;font-zie: 12px;">Occurrence:</div>
+          <div style="color: #ffffff;">Weekly</div>
+        </div>  
+      </div>
+      <span style="color: #ffffff;padding: 20px 0 15px;display: inline-block;">Available past recordings:</span>
+      <div v-if="type === 'upcoming'" class="video-wrapper" :style="{ backgroundImage: 'url(' + thumbnail_image + ')' }">
+
+      </div>
+      <div >
+        <VueSlickCarousel v-bind="settings" :arrows="true" >
+          <div class="carousel-block">
+            <div class="carouse-content">1st May</div>
+          </div>
+          <div class="carousel-block">
+            <div class="carouse-content">2nd May</div>
+          </div>
+          <div class="carousel-block">
+            <div class="carouse-content">3rd May</div>
+          </div>
+          <div class="carousel-block">
+            <div class="carouse-content">4th May</div>
+          </div>
+          <div class="carousel-block">
+            <div class="carouse-content">5th May</div>
+          </div>
+          <!-- <div class="carousel-block">
+            <div class="carouse-content">5th May</div>
+          </div> -->
+        </VueSlickCarousel>
+        </div>
+      </el-col>
+      <span slot="footer" class="dialog-footer">
+        <!-- <el-button @click="dialogVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="dialogVisible = false">Confirm</el-button> -->
+      </span>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import VueSlickCarousel from 'vue-slick-carousel'
+import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+  // optional style for arrows & dots
+  import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
+  export default {
+    name: 'EventModal',
+    components: {
+      VueSlickCarousel
+    },
+    props: {
+      type: {
+        type: String,
+        required: true
+      },
+    },
+    data() {
+      return {
+        dialogVisible: true,
+        thumbnail_image: require('../../assets/images/video-thumbnail.png'),
+        settings: {
+          dots: false,
+          centerPadding: "0px",
+          lazyLoad: "progressive",
+          focusOnSelect: true,
+          infinite: false,
+          slidesPerRow: 5,
+          rows: 1,
+          accessibility: true,
+          speed: 300,
+          edgeFriction: 0.35,
+          slidesToShow: 5,
+          slidesToScroll: 5,
+          dontAnimate: true,
+          touchThreshold: 7,
+          arrows: true,
+          variableWidth: true,
+          centerMode: true,
+          draggable: true,
+          windowWidth: null,
+          initialSlide: 1,
+        },
+      };
+    },
+    methods: {
+      handleClose() {
+        this.dialogVisible = false
+        this.$emit('close')
+        // this.$confirm('Are you sure to close this dialog?')
+        //   .then(_ => {
+        //     done();
+        //   })
+        //   .catch(_ => {});
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
