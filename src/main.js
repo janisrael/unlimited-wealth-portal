c5b566c8d25ac7e4083b31189043853b3bf34545
Jan Israel <jan.israel@smartchartsfx.com>
2023-07-28 13:05:47 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from "./store";
 import '/src/assets/styles/style.css';
 import "element-ui/lib/theme-chalk/index.css";
 import locale from "element-ui/lib/locale/lang/en"; // lang i18n
 // import "@fortawesome/fontawesome-free/css/all.css";
 import VueRouter from "vue-router";
-import { routes } from "./router/index";
+// import { routes } from "./router/index";
+
 import VueCoreVideoPlayer from 'vue-core-video-player';
 import ElementUI from "element-ui";
 import CountryFlag from 'vue-country-flag';
@@ -22,7 +24,7 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 /* add font awesome icon component */
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.use(VueRouter);
-Vue.use(VueAxios, axios)
+
 Vue.use(VueLazyload);
 Vue.use(VueLazyImageLoading, {
   cache: true,
@@ -31,15 +33,15 @@ Vue.use(VueCoreVideoPlayer)
 Vue.use(ElementUI, { locale });
 Vue.component('country-flag', CountryFlag)
 Vue.config.productionTip = false
-
-const router = new VueRouter({
-  routes,
-  mode: "hash",
-});
+Vue.use(VueAxios, axios)
+// const router = new VueRouter({
+//   routes,
+//   mode: "hash",
+// });
 
 new Vue({
   render: h => h(App),
-  router
+  store: store,
 }).$mount('#app')
 
 // Vue.component(
---
+++ src/store/index.js
@@ -0,0 +1,77 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+    namespaced: true,
+    // modules: {
+    //     lesson,
+    //     reusable,
+    //     action,
+    // },
+    state: {
+       myybookings: [],
+       myybooking_this_week: [],
+       myybooking_next_week: [],
+    },
+    mutations: {       
+        SET_MYBOOKINS: (state, payload) => {
+          state.myybookings = payload
+        },
+        ADD_BOOKING: (state, payload) => {
+          console.log(state.myybookings,'first')
+          payload.forEach(value => {
+            let event_to_add = {
+              "id": value.id,
+              "registration_key": "",
+              "start_date": "2023-07-29",
+              "status": "Attending",
+              "event_type_name": value.name,
+              "join_url": "",
+              "event_region": "aus",
+              "meta": {
+                "resource_path": "https://uw-portal-api.tinkerpub.com/api/bookings/a03Ae000003sNNhIAM"
+              }
+            }
+            state.myybookings.push(event_to_add)  
+          });
+          
+          console.log(state.myybookings,'second')
+        },
+    },
+    actions: {
+      async getMybookings({ commit, state }, value) {
+        console.log(state)
+        // state.v_loading = true;
+        return new Promise((resolve, reject) => {
+            var url = 'https://uw-portal-api.tinkerpub.com/api/my-account/bookings/upcoming'
+            axios.get(url,
+              {
+                headers: {
+                'X-Session-Key': value,
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+                }
+              }
+            )
+            .then((response) => {
+                  commit("SET_MYBOOKINS", response.data.data);
+                  resolve(response);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+        });
+      },
+      async addBooking({ commit, state }, value) {
+          console.log(state)
+          commit("ADD_BOOKING", value)
+      },
+    },
+    getters: {
+      _myybookings: (state) => state.myybookings
+    },
+});
+
+export default store;
