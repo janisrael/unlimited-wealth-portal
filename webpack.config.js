257e08d2797d77dd53613ecdbf1fb3fe5846e227
Sam Sidney <jhouel.sidney@smartchartsfx.com>
2023-08-21 21:00:18 +0800

    installed and configure sentry

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const { sentryWebpackPlugin } = require("@sentry/webpack-plugin");
+
+module.exports = {
+  // ... other config options
+  //devtool: "source-map", // Source map generation must be turned on
+  plugins: [
+    sentryWebpackPlugin({
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      org: "datageniq",
+      project: "uw-portal-app",  
+    }),
+  ],
+};
